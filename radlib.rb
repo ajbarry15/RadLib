187ad366ba609d44be4322cb2d45364e7037aa8e
BlindBatman <emlundaj@gmail.com>
2018-04-25 12:11:29 -0600

    it works

--- radlib.rb
+++ radlib.rb
@@ -3,18 +3,32 @@ require "sinatra"
 get "/" do
   erb :index
 end
+
+get "/views/index" do
+  erb :index
+end
+
 get "/upload" do
   erb :upload
 end
 
-get "/form" do
-  erb :form
+post "/upload" do
+  erb :uploadResults
+end
+
+post "/uploadResults" do
+  erb :index
 end
 
 get "/:title" do
   @title = params[:title]
+  erb :form
+end
+
+post "/:title" do
+  @title = params[:title]
   @nouns = params[:noun]
   @verbs = params[:verb]
   @adjs = params[:adj]
-  erb :form
+  erb :form2
 end
